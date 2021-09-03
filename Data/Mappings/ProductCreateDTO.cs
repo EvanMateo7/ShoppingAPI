57984bc257bf4e43c7c50b21992d6a3a25617d47
EvanMateo7 <gevanmateo@gmail.com>
2021-09-03 16:38:11 -0400

    Add data attribute to ProductCreateDTO for validation

--- Data/Mappings/ProductCreateDTO.cs
+++ Data/Mappings/ProductCreateDTO.cs
@@ -1,19 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShoppingAPI.Data.Mappings
 {
   public class ProductCreateDTO
-    {
-        public string UserId { get; set; }
+  {
+    [Required]
+    public string UserId { get; set; }
 
-        public string Name { get; set; }
+    [Required]
+    public string Name { get; set; }
 
 #nullable enable
-        public string? Description { get; set; }
+    public string? Description { get; set; }
 #nullable disable
 
-        public float Quantity { get; set; }
+    [Range(0, Int32.MaxValue, ErrorMessage = "quantity should not be less than zero.")]
+    public float Quantity { get; set; }
 
-        public decimal Price { get; set; }
-    }
+    [Range(0, Int32.MaxValue, ErrorMessage = "price should not be less than zero.")]
+    public decimal Price { get; set; }
+  }
 }
