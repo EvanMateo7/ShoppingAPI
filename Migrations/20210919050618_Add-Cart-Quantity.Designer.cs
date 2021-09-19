9c5b0fdaaeb96875a315360afb953d2ac7e83db4
EvanMateo7 <gevanmateo@gmail.com>
2021-09-19 02:20:30 -0400

    Add Cart Quantity migration

--- Migrations/20210919050618_Add-Cart-Quantity.Designer.cs
+++ Migrations/20210919050618_Add-Cart-Quantity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ShoppingAPI.Data;
 
 namespace ShoppingAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210919050618_Add-Cart-Quantity")]
+    partial class AddCartQuantity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
